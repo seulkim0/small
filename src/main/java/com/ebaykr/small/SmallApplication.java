3105929e5486e3d834d067dc49c0e10983b59879
hgwag <hgwag@ebay.com>
2018-06-05 18:59:04 +0900

    1st commit

---
+++ src/main/java/com/ebaykr/small/SmallApplication.java
@@ -0,0 +1,12 @@
+package com.ebaykr.small;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallApplication.class, args);
+	}
+}
